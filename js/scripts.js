85fb316082af169639e820b7598cefb35b910378
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 14:03:07 -0700

    add variables in js to take in form data

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
- $(form#appointment).submit(function(){
+ $('form#appointment').submit(function(event){
+  event.preventDefault();
+
+  var name =
+  var description =
+  var date =
+  var start =
+  var end =
 
 
 
-
-  
  });
 });
