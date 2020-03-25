84733ccb5d823fa0856473f602bfcccddbdde0e9
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 14:13:58 -0700

    give form input id"s, place each input and label in unique div

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
  $('form#appointment').submit(function(event){
   event.preventDefault();
 
-  var name =
-  var description =
-  var date =
-  var start =
-  var end =
+  var name =$('#name').val();
+  var description = $('#description').val();
+  var date = $('#date').val();
+  var start = $('#start').val();
+  var end = $('#end').val();
 
 
 
